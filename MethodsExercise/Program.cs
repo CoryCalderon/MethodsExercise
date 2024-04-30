2e90ce84e41595e9b383b514ed87b15c6c7c6bcf
Cory <corycalderon95@gmail.com>
2024-04-30 16:13:53 -0400

    Completed assignment and challenge mode

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace MethodsExercise
+﻿using System.Net.NetworkInformation;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What is a name for a dog?");
+            string dogName = Console.ReadLine();
+            Console.WriteLine("What color is your dog?");
+            string dogColor = Console.ReadLine();
+
+            Console.WriteLine($"There was a man named {userName}. He was {age} years old.");
+            Console.WriteLine($"He had a {dogColor} dog named {dogName}");
+            Console.WriteLine($"{userName} loved {dogName}. But he hated being {age}.");
+
+
+            Console.WriteLine("Gime a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: (sum)");
+        }
+        public static int Sum(params int[] list)
+        {
+            int sum = 0;
+
+            for(int i = 0; i<list.Length; i++)
+            {
+                sum = sum + list[i];
+            }
+            return sum;
+
+        }
+        public static int Multiply(int x, int y)
+        {
+
+            return x * y;
         }
     }
 }
