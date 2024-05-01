813bbc8789f530d4d84f71b7a72a3b82c491a83f
Cory <corycalderon95@gmail.com>
2024-05-01 11:46:07 -0400

    Revised

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -6,6 +6,23 @@ namespace MethodsExercise
     {
         static void Main(string[] args)
         {
+            int added = Add(2, 2);
+            Console.WriteLine(added);
+
+            int subtracted = Subtract(10, 5);
+            Console.WriteLine(subtracted);
+
+            int mult = Multiply(2, 5);
+            Console.WriteLine(mult);
+
+            int div = Divide(4, 2);
+            Console.WriteLine(div);
+
+            MadLib();
+        }
+
+        public static void MadLib()
+        {
             Console.WriteLine("What is your name?");
             string userName = Console.ReadLine();
             Console.WriteLine("What is your age?");
@@ -27,23 +44,35 @@ namespace MethodsExercise
             int num2 = int.Parse(Console.ReadLine());
 
             int sum = Sum(num1, num2);
-            Console.WriteLine($"The sum is: (sum)");
+            Console.WriteLine($"The sum is: {sum}");
         }
         public static int Sum(params int[] list)
         {
             int sum = 0;
 
-            for(int i = 0; i<list.Length; i++)
+            for (int i = 0; i < list.Length; i++)
             {
                 sum = sum + list[i];
             }
             return sum;
 
         }
-        public static int Multiply(int x, int y)
+       
+        public static int Add(int num1, int num2)
         {
-
-            return x * y;
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        {  
+            return (num1 / num2);
         }
     }
 }
